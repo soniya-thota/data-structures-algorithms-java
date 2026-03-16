1518076cf6b6cdf7d10257bdb5ff8c70c00584c1
Soniya <126555086+soniya-thota@users.noreply.github.com>
2026-03-15 21:29:50 -0400

    Implement inorder traversal for binary tree

--- trees/BinaryTreeTraversal.java
+++ trees/BinaryTreeTraversal.java
@@ -1,1 +1,24 @@
+class TreeNode {
 
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+
+public class BinaryTreeTraversal {
+
+    public static void inorder(TreeNode root){
+
+        if(root == null) return;
+
+        inorder(root.left);
+
+        System.out.print(root.val + " ");
+
+        inorder(root.right);
+    }
+}
