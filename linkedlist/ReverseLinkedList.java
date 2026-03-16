dc04421de4d1cfdea59f2d509f5bfa99561c651a
Soniya <126555086+soniya-thota@users.noreply.github.com>
2026-03-15 21:25:01 -0400

    Add reverseList method to ReverseLinkedList class
    Implemented a method to reverse a linked list.

--- linkedlist/ReverseLinkedList.java
+++ linkedlist/ReverseLinkedList.java
@@ -1,1 +1,31 @@
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current != null) {
+
+            ListNode next = current.next;
+
+            current.next = prev;
+
+            prev = current;
+
+            current = next;
+        }
+
+        return prev;
+    }
+}
