9541c4190779d22760b12201a52d589b49dd045d
Soniya <126555086+soniya-thota@users.noreply.github.com>
2026-03-15 21:30:16 -0400

    Add BFS implementation in BFS_DFS.java
    Implement BFS algorithm for graph traversal.

--- graphs/BFS_DFS.java
+++ graphs/BFS_DFS.java
@@ -1,1 +1,30 @@
 
+import java.util.*;
+
+public class BFS_DFS {
+
+    static void bfs(int start, List<List<Integer>> graph) {
+
+        boolean[] visited = new boolean[graph.size()];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()) {
+
+            int node = queue.poll();
+
+            System.out.print(node + " ");
+
+            for(int neighbor : graph.get(node)) {
+
+                if(!visited[neighbor]) {
+
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
